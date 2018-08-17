b91c104202112b66ee94837dcdec7532adc872d8
Eduardo Luz <eduluz1976@gmail.com>
2018-08-17 01:13:52 +0000

    Adding User, and updating composer.json

---
+++ src/User.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace eduluz1976\user_management;
+
+/**
+ * Class User
+ * @package eduluz1976\user_management
+ */
+class User
+{
+
+    protected $username;
+    protected $hash;
+    protected $email;
+    protected $status;
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @param string $hash
+     * @return User
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     * @return User
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+
+}
